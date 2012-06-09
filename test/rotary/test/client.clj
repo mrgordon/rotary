42bd67f5a477d2bef52ea78232f2497a2d0b7e62
Matthew Gordon <matt.gordon@crowdflower.com>
2012-06-09 12:56:04 -0700

    Modify tests to support new query syntax

--- test/rotary/test/client.clj
+++ test/rotary/test/client.clj
@@ -11,34 +11,34 @@
 ; hash-key 25, range-key 1339202645
 
 (deftest query-with-range-key-can-find-rows
-  (is (count (query aws-credential test-table 25 {:range-key 1339 :operator "GT"})) 1))
+  (is (count (query aws-credential test-table 25 `(> 1339))) 1))
 
 (deftest query-with-range-key-can-exclude-rows
-  (is (count (query aws-credential test-table 25 {:range-key 13392029999 :operator "GT"})) 0))
+  (is (count (query aws-credential test-table 25 `(> 13392029999))) 0))
 
 (deftest query-with-range-key-can-handle-greater-than
-  (is (count (query aws-credential test-table 22 {:range-key 1339202642 :operator "GT"})) 1))
+  (is (count (query aws-credential test-table 22 `(> 1339202642))) 1))
 
 (deftest query-with-range-key-can-handle-less-than
-  (is (count (query aws-credential test-table 22 {:range-key 1339202642 :operator "LT"})) 1))
+  (is (count (query aws-credential test-table 22 `(< 1339202642))) 1))
 
 (deftest query-with-range-key-less-than
-  (is (count (query aws-credential test-table 22 {:range-key 1339202640 :operator "LT"})) 0))
+  (is (count (query aws-credential test-table 22 `(< 1339202640))) 0))
 
 (deftest query-with-range-key-less-than-or-equals-and-count
-  (is (query aws-credential test-table 22 {:range-key 1339202640 :operator "LE" :count true}) 1))
+  (is (query aws-credential test-table 22 `(<= 1339202640) {:count true}) 1))
 
 (deftest query-with-range-key-can-handle-greater-than-with-multiple-items
-  (is (query aws-credential test-table 22 {:range-key 13392 :operator "GT" :count true}) 2))
+  (is (query aws-credential test-table 22 `(> 13392) {:count true}) 2))
 
 (deftest query-with-range-key-can-handle-greater-than-with-multiple-items-and-limit
-  (is (query aws-credential test-table 22 {:range-key 13392 :operator "GT" :limit 1 :count true})) 1)
+  (is (query aws-credential test-table 22 `(> 13392) {:count true :limit 1}) 1))
 
 (deftest query-with-range-key-can-exclude-with-between
-  (is (count (query aws-credential test-table 22 {:range-key 13392 :operator "BETWEEN" :range-end 13399})) 0))
+  (is (count (query aws-credential test-table 22 `("BETWEEN" 13392 13399))) 0))
 
 (deftest query-with-range-key-can-include-with-between
-  (is (count (query aws-credential test-table 22 {:range-key 13392 :operator "BETWEEN" :range-end 999999999})) 2))
+  (is (count (query aws-credential test-table 22 `("BETWEEN" 13392 999999999))) 2))
 
 (deftest query-with-range-key-can-include-and-exclude-with-between
-  (is (count (query aws-credential test-table 22 {:range-key 13392 :operator "BETWEEN" :range-end 1339202643})) 1))
+  (is (count (query aws-credential test-table 22 `("BETWEEN" 13392 1339202643))) 1))
