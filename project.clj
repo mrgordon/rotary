f603097c4df6e09af9e0e595611cbe85bc02f502
Matthew Gordon <matt.gordon@crowdflower.com>
2012-06-09 01:38:31 -0700

    Version bump for this project and dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rotary "0.2.3"
+(defproject rotary "0.2.4"
   :description "Amazon DynamoDB API"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/algo.generic "0.1.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.6"]]
+                 [com.amazonaws/aws-java-sdk "1.3.10"]]
   :plugins [[codox "0.6.1"]])
