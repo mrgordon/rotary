210d6ee07ad04d6d6dda2e8882599837eabf469e
Ivan Toshkov <ivan@toshkov.org>
2012-06-20 10:20:36 +0300

    Incompatible (but more powerful and cosistent) iterface

--- src/rotary/client.clj
+++ src/rotary/client.clj
@@ -11,10 +11,12 @@
             BatchWriteItemResult
             Condition
             CreateTableRequest
+            DeleteItemRequest
+            DeleteItemResult
+            DeleteTableRequest
             DescribeTableRequest
             DescribeTableResult
-            DeleteTableRequest
-            DeleteItemRequest
+            ExpectedAttributeValue
             GetItemRequest
             GetItemResult
             Key
@@ -23,6 +25,7 @@
             ProvisionedThroughput
             ProvisionedThroughputDescription
             PutItemRequest
+            PutItemResult
             PutRequest
             QueryRequest
             ResourceNotFoundException
@@ -107,14 +110,15 @@
   DescribeTableResult
   (as-map [result]
     (let [table (.getTable result)]
-      {:name          (.getTableName table)
-       :creation-date (.getCreationDateTime table)
-       :item-count    (.getItemCount table)
-       :key-schema    (as-map (.getKeySchema table))
-       :throughput    (as-map (.getProvisionedThroughput table))
-       :status        (-> (.getTableStatus table)
-                          (str/lower-case)
-                          (keyword))})))
+      {:name             (.getTableName table)
+       :creation-date    (.getCreationDateTime table)
+       :item-count       (.getItemCount table)
+       :table-size-bytes (.getTableSizeBytes table)
+       :key-schema       (as-map (.getKeySchema table))
+       :throughput       (as-map (.getProvisionedThroughput table))
+       :status           (-> (.getTableStatus table)
+                             (str/lower-case)
+                             (keyword))})))
 
 (defn describe-table
   "Returns a map describing the table in DynamoDB with the given name, or nil
@@ -188,6 +192,19 @@
       (if-let [v (.getNS attr-value)] (into #{} v))
       (if-let [v (.getSS attr-value)] (into #{} v))))
 
+(defn- to-expected-value
+  "Convert a value to an ExpcetedValue object. Handles ::exists
+  and ::not-exists specially."
+  [value]
+  (cond (= value ::exists) (ExpectedAttributeValue. true)
+        (= value ::not-exists) (ExpectedAttributeValue. false)
+        :else (if-let [av (to-attr-value value)] (ExpectedAttributeValue. av))))
+
+(defn- to-expected-values
+  [values]
+  (and (seq values)
+       (fmap to-expected-value values)))
+
 (defn- item-map
   "Turn a item in DynamoDB into a Clojure map."
   [item]
@@ -197,16 +214,35 @@
 (extend-protocol AsMap
   GetItemResult
   (as-map [result]
-    (item-map (.getItem result))))
+    (with-meta
+      (item-map (.getItem result))
+      {:consumed-capacity-units (.getConsumedCapacityUnits result)}))
+
+  PutItemResult
+  (as-map [result]
+    (with-meta
+      (item-map (or (.getAttributes result) {}))
+      {:consumed-capacity-units (.getConsumedCapacityUnits result)}))
+
+  DeleteItemResult
+  (as-map [result]
+    (with-meta
+      (item-map (or (.getAttributes result) {}))
+      {:consumed-capacity-units (.getConsumedCapacityUnits result)})))
 
 (defn put-item
   "Add an item (a Clojure map) to a DynamoDB table."
-  [cred table item]
-  (.putItem
-   (db-client cred)
-   (doto (PutItemRequest.)
-     (.setTableName table)
-     (.setItem (fmap to-attr-value item)))))
+  ;; TODO Add documnetation about "expected" and "return-values"
+  ;; TODO Consider using keywords for the "return-values"
+  [cred table item & {:keys [expected return-values]}]
+  (as-map
+   (.putItem
+    (db-client cred)
+    (doto (PutItemRequest.)
+      (.setTableName table)
+      (.setItem (fmap to-attr-value item))
+      (.setExpected (to-expected-values expected))
+      (.setReturnValues return-values)))))
 
 (defn- to-put-request [item]
   "Transforms an item (a Clojure map) into a PutRequest"
@@ -249,52 +285,67 @@
 
 (defn- item-key
   "Create a Key object from a value."
-  ([hash-key]
-     (item-key hash-key nil))
+  ([[hash-key range-key :as key]]
+     (cond
+         (nil? key) nil
+         (vector? key) (item-key hash-key range-key)
+         :else (item-key key nil)))
   ([hash-key range-key]
      (Key. (to-attr-value hash-key)
            (to-attr-value range-key))))
 
+(defn- decode-key [k]
+  (let [hash (.getHashKeyElement k)
+        range (.getRangeKeyElement k)]
+    (if range
+      [(get-value hash) (get-value range)]
+      (get-value hash))))
+
 (defn update-item
   "Update an item (a Clojure map) in a DynamoDB table."
-  [cred table hash-key range-key & update-map]
+  ;; TODO Add documnetation about "update-map"
+  ;; TODO Add documnetation about "expected" and "return-values"
+  ;; TODO Consider using keywords for the "return-values"
+  [cred table [hash-key range-key :as key] update-map & {:keys [expected return-values]}]
   (let [attribute-update-map (fmap to-attr-value-update (apply hash-map update-map))]
     (.updateItem
      (db-client cred)
      (doto (UpdateItemRequest.)
        (.setTableName table)
-       (.setKey (item-key hash-key range-key))
-       (.setAttributeUpdates attribute-update-map)))))
+       (.setKey (item-key key))
+       (.setAttributeUpdates attribute-update-map)
+       (.setExpected (to-expected-values expected))
+       (.setReturnValues return-values)))))
 
 (defn get-item
-  "Retrieve an item from a DynamoDB table by its hash key."
-  ([cred table hash-key]
-     (get-item cred table hash-key nil))
-  ([cred table hash-key range-key]
-     (as-map
-      (.getItem
-       (db-client cred)
-       (doto (GetItemRequest.)
-         (.setTableName table)
-         (.setKey (item-key hash-key range-key)))))))
+  "Retrieve an item from a DynamoDB table by its key.
+  The key can be given in the forms:
+   * \"hash\"
+   * [\"hash\"]
+   * [\"hash\" \"range\"]"
+  ;; TODO Add documnetation about "consistent-read" and "attributes-to-get"
+  [cred table [hash-key range-key :as key] & {:keys [consistent-read attributes-to-get] :or {consistent-read false}}]
+  (as-map
+   (.getItem
+    (db-client cred)
+    (doto (GetItemRequest.)
+      (.setTableName table)
+      (.setKey (item-key key))
+      (.setConsistentRead consistent-read)
+      (.setAttributesToGet attributes-to-get)))))
 
 (defn delete-item
-  "Delete an item from a DynamoDB table by its hash key."
-  ([cred table hash-key]
-     (delete-item cred table hash-key nil))
-  ([cred table hash-key range-key]
-     (.deleteItem
-      (db-client cred)
-      (DeleteItemRequest. table (item-key hash-key range-key)))))
-
-(defn scan
-  "Return the items in a DynamoDB table."
-  [cred table]
-  (map item-map
-       (.getItems
-        (.scan
-         (db-client cred)
-         (ScanRequest. table)))))
+  "Delete an item from a DynamoDB table specified by its key(s), if
+  the coditions are met."
+  ;; TODO Add documnetation about "expected" and "return-values"
+  ;; TODO Consider using keywords for the "return-values"
+  [cred table [hash-key range-key :as key] & {:keys [expected return-values]}]
+  (as-map
+   (.deleteItem
+    (db-client cred)
+    (doto (DeleteItemRequest. table (item-key key))
+      (.setExpected (to-expected-values expected))
+      (.setReturnValues return-values)))))
 
 (defn- set-range-condition
   "Add the range key condition to a QueryRequest object"
@@ -307,7 +358,7 @@
 
 (defn- query-request
   "Create a QueryRequest object."
-  [table hash-key range-clause {:keys [order limit count consistent]}]
+  [table hash-key range-clause {:keys [order limit count consistent exclusive-start-key]}]
   (let [qr (QueryRequest. table (to-attr-value hash-key))
         [operator range-key range-end] range-clause]
     (when operator
@@ -320,6 +371,8 @@
       (.setCount qr count))
     (when consistent
       (.setConsistentRead qr consistent))
+    (when exclusive-start-key
+      (.setExclusiveStartKey qr (item-key exclusive-start-key)))
     qr))
 
 (defn query
@@ -329,11 +382,29 @@
     :order - may be :asc or :desc (defaults to :asc)
     :limit - should be a positive integer
     :count - return a count if logical true
-    :consistent - return a consistent read if logical true"
-  [cred table hash-key & [range-clause options]]
+    :consistent - return a consistent read if logical true
+    :exclusive-start-key - primary key of the item from which to continue an earlier query"
+  [cred table hash-key range-clause & {:keys [order limit count consistent exclusive-start-key] :as options}]
   (let [query-result (.query
                       (db-client cred)
                       (query-request table hash-key range-clause options))]
-    (if (get options :count)
-      (.getCount query-result)
-      (map item-map (.getItems query-result)))))
+    (with-meta
+      (map item-map (or (.getItems query-result) {}))
+      (merge {:count (.getCount query-result)
+              :consumed-capacity-units (.getConsumedCapacityUnits query-result)}
+             (if-let [k (.getLastEvaluatedKey query-result)]
+               {:last-evaluated-key (decode-key k)})))))
+
+(defn scan
+  "Return the items in a DynamoDB table."
+  [cred table & {:keys [attributes-to-get count exclusive-start-key limit scan-filter]}]
+  (map item-map
+       (.getItems
+        (.scan
+         (db-client cred)
+         (doto (ScanRequest. table)
+           (.setAttributesToGet attributes-to-get)
+           (.setCount count)
+           (.setExclusiveStartKey (item-key exclusive-start-key))
+           (.setLimit limit)
+           )))))
