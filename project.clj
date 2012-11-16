7e38fbf530fcb9a03a1e418cf4b33a5f3438002a
Matthew Gordon <matt.gordon@crowdflower.com>
2012-11-16 13:00:32 -0800

    Update AWS SDK version because it contains DynamoDB request fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject org.clojars.crowdflower/rotary "0.3.7"
+(defproject org.clojars.crowdflower/rotary "0.3.8"
   :description "Amazon DynamoDB API"
+  :url "https://github.com/CrowdFlower/rotary"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/algo.generic "0.1.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.21"]]
+                 [com.amazonaws/aws-java-sdk "1.3.24"]]
   :plugins [[codox "0.6.1"]])
